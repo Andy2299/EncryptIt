8ea07fce0224225c8d2e2422f829866ba98ac733
Andy2299 <90882478+Andy2299@users.noreply.github.com>
2023-08-25 12:10:45 -0600

    Tamagotchi

--- tamagotchi.py
+++ tamagotchi.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import time
 
 class Tamagotchi:
     def __init__(self, name):
@@ -7,33 +8,18 @@ class Tamagotchi:
         self.hunger = 10
         self.tiredness = 10
         self.happiness = 10
-        self.pet = """
-        ^_^
-       /   \\
-      (     )
-       \\___/
-        """
 
-    def eat(self):
-        if self.hunger > 0:
-            self.hunger -= 1
+    def get_pet(self):
+        if self.happiness > 7:
+            return "^_^"
+        elif self.hunger > 7:
+            return "o_o"
         else:
-            messagebox.showinfo("Información", f"{self.name} no tiene hambre.")
-
-    def sleep(self):
-        if self.tiredness > 0:
-            self.tiredness -= 1
-        else:
-            messagebox.showinfo("Información", f"{self.name} no tiene sueño.")
-
-    def play(self):
-        if self.happiness < 10:
-            self.happiness += 1
-        else:
-            messagebox.showinfo("Información", f"{self.name} ya está muy feliz.")
+            return "-_-"
 
     def show_status(self):
-        return f"{self.pet}\n{self.name} - Hambre: {self.hunger}, Sueño: {self.tiredness}, Felicidad: {self.happiness}"
+        pet = self.get_pet()
+        return f"{pet}\n{self.name} - Hambre: {self.hunger}, Sueño: {self.tiredness}, Felicidad: {self.happiness}"
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -63,18 +49,21 @@ class Application(tk.Frame):
         self.play.pack(side="left")
 
     def feed(self):
-        self.tamagotchi.eat()
-        self.status["text"] = self.tamagotchi.show_status()
+        self.tamagotchi.hunger = min(10, self.tamagotchi.hunger + 1)
+        self.update_status()
 
     def rest(self):
-        self.tamagotchi.sleep()
-        self.status["text"] = self.tamagotchi.show_status()
+        self.tamagotchi.tiredness = min(10, self.tamagotchi.tiredness + 1)
+        self.update_status()
 
     def game(self):
-        self.tamagotchi.play()
+        self.tamagotchi.happiness = min(10, self.tamagotchi.happiness + 1)
+        self.update_status()
+
+    def update_status(self):
         self.status["text"] = self.tamagotchi.show_status()
 
-def main():
+if __name__ == '__main__':
     root = tk.Tk()
     app = Application(master=root)
     app.mainloop()
