6f7f88ed93061d27a7e8f5ee4e37669c6ddbf9c3
Andy2299 <90882478+Andy2299@users.noreply.github.com>
2023-08-25 13:06:12 -0600

    animaciones

--- tamagotchi.py
+++ tamagotchi.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
-import time
 
+# Tamagotchi class definition
 class Tamagotchi:
     def __init__(self, name):
         self.name = name
@@ -21,6 +21,7 @@ class Tamagotchi:
         pet = self.get_pet()
         return f"{pet}\n{self.name} - Hambre: {self.hunger}, Sueño: {self.tiredness}, Felicidad: {self.happiness}"
 
+# GUI Application class definition
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -30,22 +31,16 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.status = tk.Label(self, text=self.tamagotchi.show_status(), font=("Courier", 10))
+        self.status = tk.Label(self, text=self.tamagotchi.show_status(), font=("Courier", 14))
         self.status.pack(side="top")
 
-        self.eat = tk.Button(self)
-        self.eat["text"] = "Comer"
-        self.eat["command"] = self.feed
+        self.eat = tk.Button(self, text="Comer", command=self.feed)
         self.eat.pack(side="left")
 
-        self.sleep = tk.Button(self)
-        self.sleep["text"] = "Dormir"
-        self.sleep["command"] = self.rest
+        self.sleep = tk.Button(self, text="Dormir", command=self.rest)
         self.sleep.pack(side="left")
 
-        self.play = tk.Button(self)
-        self.play["text"] = "Jugar"
-        self.play["command"] = self.game
+        self.play = tk.Button(self, text="Jugar", command=self.game)
         self.play.pack(side="left")
 
     def feed(self):
@@ -61,8 +56,9 @@ class Application(tk.Frame):
         self.update_status()
 
     def update_status(self):
-        self.status["text"] = self.tamagotchi.show_status()
+        self.status.config(text=self.tamagotchi.show_status())
 
+# Main function to run the application
 if __name__ == '__main__':
     root = tk.Tk()
     app = Application(master=root)
